9aae6eb0b4e02b701d0919ad0c9a29f3e3ff1205
Avdeev Denis <av-denis@yandex-team.ru>
2020-08-23 00:03:47 +0300

    01-intro/1-sum: решение

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -2,8 +2,13 @@
  * sum
  * @param {number} m base
  * @param {number} n index
- * @returns {number}
+ * @returns {number|null}
  */
 export default function sum(m, n) {
+    if (
+        typeof(m) !== 'number' ||
+        typeof(n) !== 'number'
+    ) return null;
 
+    return m + n;
 }
