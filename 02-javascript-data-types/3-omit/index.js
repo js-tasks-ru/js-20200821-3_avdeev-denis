355450ef1de282188997ba32dcb408c840394ae9
Avdeev Denis <av-denis@yandex-team.ru>
2020-08-26 01:07:31 +0300

    02-javascript-data-types/3-omit решение задачи

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    const newObject = Object.assign({}, obj);
 
+    return fields.reduce((result, field) => {
+        if (result[field]) {
+            delete result[field];
+        }
+        
+        return result;
+    }, newObject);
 };
