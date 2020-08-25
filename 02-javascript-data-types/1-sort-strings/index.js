608042d40bcf9ed83bdb97952ab6ba48aa190c91
Avdeev Denis <av-denis@yandex-team.ru>
2020-08-26 01:48:23 +0300

    02-javascript-data-types/1-sort-strings решение задачи

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,32 @@
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
- * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
+ * @param {string} [sortType="asc"] sortType - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+/**
+ * Возможные варианты сортировки
+ */
+const AVALIABLE_SORT_TYPES = ['desc', 'asc'];
+
+/**
+ * Поддерживаемые языки (http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry)
+ */
+const SUPPORTED_LANGS = ['en', 'ru'];
+
+export function sortStrings(arr, sortType = 'asc') {
+    if (!AVALIABLE_SORT_TYPES.includes(sortType)) {
+        throw new Error('Неправильный тип сортировки, возможные значения ' + AVALIABLE_SORT_TYPES.join(', '));
+    }
+
+    const newArray = [].concat(arr);
+
+    return newArray.sort((a, b) => {
+        const comparedPairs = sortType === 'asc' ?
+            [a, b] :
+            [b, a];
+
+        return new Intl.Collator(SUPPORTED_LANGS, { caseFirst: 'upper' }).compare(...comparedPairs);
+    });
 }
