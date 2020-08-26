c44e56d6ad99818efb40ec896e876110825d9ad0
Avdeev Denis <av-denis@yandex-team.ru>
2020-08-26 21:07:26 +0300

    fixup! 02-javascript-data-types/1-sort-strings решение задачи

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,29 +4,32 @@
  * @param {string} [sortType="asc"] sortType - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-
-/**
- * Возможные варианты сортировки
- */
-const AVALIABLE_SORT_TYPES = ['desc', 'asc'];
-
-/**
- * Поддерживаемые языки (http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry)
- */
-const SUPPORTED_LANGS = ['en', 'ru'];
-
 export function sortStrings(arr, sortType = 'asc') {
+    /**
+     * Возможные варианты сортировки
+     */
+    const AVALIABLE_SORT_TYPES = ['desc', 'asc'];
+
     if (!AVALIABLE_SORT_TYPES.includes(sortType)) {
         throw new Error('Неправильный тип сортировки, возможные значения ' + AVALIABLE_SORT_TYPES.join(', '));
     }
 
-    const newArray = [].concat(arr);
+    /**
+     * Поддерживаемые языки (http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry)
+     */
+    const SUPPORTED_LANGS = ['en', 'ru'];
 
-    return newArray.sort((a, b) => {
-        const comparedPairs = sortType === 'asc' ?
-            [a, b] :
-            [b, a];
+    return [...arr].sort((a, b) => {
+        let value1, value2;
 
-        return new Intl.Collator(SUPPORTED_LANGS, { caseFirst: 'upper' }).compare(...comparedPairs);
+        if (sortType === 'asc') {
+            value1 = a;
+            value2 = b;
+        } else if (sortType === 'desc') {
+            value1 = b;
+            value2 = a;
+        }
+
+        return value1.localeCompare(value2, SUPPORTED_LANGS, { caseFirst: 'upper' });
     });
 }
