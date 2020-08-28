d1ccb226dbf6f94ab47520732acbab9c921f6f27
Avdeev Denis <av-denis@yandex-team.ru>
2020-08-29 01:10:26 +0300

    03-objects-arrays-intro-to-testing/1-create-getter решение задачи

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    function getter(obj) {
+        const parts = path.split('.');
 
+        let result = obj;
+
+        while (parts.length) {
+            const nestedItem = result[parts.shift()];
+
+            if (!nestedItem) return;
+
+            result = nestedItem;
+        }
+
+        return result;
+    }
+
+    return getter;
 }
