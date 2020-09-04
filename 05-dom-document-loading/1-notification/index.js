df428d8d189445c4580d4f3607370612a57c8e2e
Avdeev Denis <av-denis@yandex-team.ru>
2020-09-05 02:32:03 +0300

    05-dom-document-loading/1-notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,74 @@
 export default class NotificationMessage {
+    element;
 
+    static isShowed = false; // показано ли в данный момент оповещение
+    
+    constructor(body, { duration = 2000, type = 'success' } = {}) {
+        this.body = body;
+
+        this.duration = duration;
+        this.type = type;
+
+        this.element = this.getHTMLFromTemplate(this.getTemplate());
+    }
+    
+    setNotificationShowed() {
+        NotificationMessage.isShowed = true;
+    }
+    
+    setNotificationHidden() {
+        NotificationMessage.isShowed = false;
+    }
+    
+    show(target) {
+        if (NotificationMessage.isShowed) return;
+
+        this.setNotificationShowed();
+        
+        this.showTimeouted(target);
+    }
+
+    showTimeouted(target) {
+        target?.append(this.element);
+
+        setTimeout(() => {
+            this.remove();
+
+            this.setNotificationHidden();
+        }, this.duration);
+    }
+
+    getHTMLFromTemplate(template) {
+        const parentNode = document.createElement('div');
+
+        parentNode.innerHTML = template;
+
+        return parentNode.firstElementChild;
+    }
+
+    getTimerValue() {
+        return (this.duration / 1000) + 's';
+    }
+
+    getTemplate() {
+        return `
+            <div class="notification ${this.type}" style="--value:${this.getTimerValue()}">
+                <div class="timer"></div>
+                    <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.body}
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    destroy() {
+        this.remove();
+    }
+
+    remove() {
+        this.element.remove();
+    }
 }
